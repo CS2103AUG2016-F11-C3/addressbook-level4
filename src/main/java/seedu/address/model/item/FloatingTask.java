54eeed0115046a759a95a5a749bd8510311c3eae
acuodancer <leminhduc74@gmail.com>
2016-10-07 14:11:40 +0800

    Add a copy constructor that take a ReadOnlyItem as the source to create FloatingTask

--- src/main/java/seedu/address/model/item/FloatingTask.java
+++ src/main/java/seedu/address/model/item/FloatingTask.java
@@ -7,6 +7,13 @@ public class FloatingTask extends Item implements ReadOnlyItem {
 	public FloatingTask(Description desc) {
 		super(desc);
 	}
+	
+	/**
+     * Copy constructor.
+     */
+    public FloatingTask(ReadOnlyItem source) {
+        this(source.getDescription());
+    }
 
 
 	@Override
