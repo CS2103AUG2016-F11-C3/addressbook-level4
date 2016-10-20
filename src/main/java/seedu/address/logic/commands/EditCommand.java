eafa22db2ae3b6adfd0134079fb3a4e9dadf6294
Darren Wee <darren.wee@u.nus.edu>
2016-10-21 04:53:19 +0800

    Make messages more helpful and extend ALLOWED_FIELDS

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -18,15 +18,16 @@ public class EditCommand extends Command {
 
 	public static final String COMMAND_WORD = "for";
 
-	public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits an existing item. " + "CONTEXT_ID FIELD:NEW"
-			+ "Example: " + COMMAND_WORD + "2 start:2000 on 07/10/2016";
+	public static final String MESSAGE_USAGE = COMMAND_WORD + "... edit ..." + ": Edits an existing item.\n"
+	        + "Syntax: for CONTEXT_ID edit FIELD:NEW_DETAIL\n"
+			+ "Example: " + COMMAND_WORD + " 2 edit start:2000 on 07/10/2016";
 
 	public static final String MESSAGE_SUCCESS = "Successfully edited: %1$s";
 	public static final String MESSAGE_SUCCESS_TIME_NULL = "START or END time not found but new task added!";
 	public static final String MESSAGE_DUPLICATE_ITEM = "This task already exists in the to-do list";
-	public static final String MESSAGE_INVALID_FIELD = "The available fields are: desc/description,start and end";
+	public static final String MESSAGE_INVALID_FIELD = "The available fields are: desc/description, start, end, by and period";
 
-	public static final String[] ALLOWED_FIELDS = { "desc", "description", "start", "end" };
+	public static final String[] ALLOWED_FIELDS = { "desc", "description", "start", "end", "by", "period" };
 
 	private static final String DEFAULT_ITEM_NAME = "BLOCK";
 	
