7509fb51b68ebc38cc6c70a10c8b0c122b6c81f7
acuodancer <leminhduc74@gmail.com>
2016-10-27 02:19:52 +0800

    Set hasUndo to false because Undo cannot be undone (for now)

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -1,5 +1,18 @@
 package seedu.address.logic.commands;
 
-public class UndoCommand {
+public class UndoCommand extends Command {
+	
+	public static final String MESSAGE_UNDO_FAILURE = "";
+
+	@Override
+	public CommandResult execute() {
+		hasUndo = false;
+		return null;
+	}
+
+	@Override
+    public CommandResult undo() {
+        return new CommandResult(MESSAGE_UNDO_FAILURE);
+    }
 
 }
