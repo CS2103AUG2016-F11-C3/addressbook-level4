54dab8bdfe4ba946fc24dd252f0796448040c670
acuodancer <leminhduc74@gmail.com>
2016-10-27 02:46:37 +0800

    Add COMMAND_WORD

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 public class UndoCommand extends Command {
 	
 	public static final String MESSAGE_UNDO_FAILURE = "";
+	public static final String COMMAND_WORD = "undo";
 
 	@Override
 	public CommandResult execute() {
