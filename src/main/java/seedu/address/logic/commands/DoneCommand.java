c3145584ca5d83755c8ca263a036bbaf80a42526
acuodancer <leminhduc74@gmail.com>
2016-10-27 01:01:00 +0800

    Add undo() method to DoneCommand to set previous done item to not done

--- src/main/java/seedu/address/logic/commands/DoneCommand.java
+++ src/main/java/seedu/address/logic/commands/DoneCommand.java
@@ -7,44 +7,53 @@ import seedu.address.model.item.Item;
 import seedu.address.model.item.ReadOnlyItem;
 
 public class DoneCommand extends Command {
-    public static final String COMMAND_WORD = "done";
+	public static final String COMMAND_WORD = "done";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Marks the task identified by index as done.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example 1: " + COMMAND_WORD + " 1\n";
+	public static final String MESSAGE_UNDO_SUCCESS = "Undo set done task: %1$s";
+	public static final String MESSAGE_UNDO_FAILURE = "";
 
-    public static final String MESSAGE_DONE_ITEM_SUCCESS = "Task marked as complete!";
-    public static final String MESSAGE_DONE_ITEM_FAIL = "Task already marked as complete!";
+	public static final String MESSAGE_USAGE = COMMAND_WORD + ": Marks the task identified by index as done.\n"
+			+ "Parameters: INDEX (must be a positive integer)\n" + "Example 1: " + COMMAND_WORD + " 1\n";
 
-    public final int targetIndex;
+	public static final String MESSAGE_DONE_ITEM_SUCCESS = "Task marked as complete!";
+	public static final String MESSAGE_DONE_ITEM_FAIL = "Task already marked as complete!";
 
-    public DoneCommand(int index) {
-        this.targetIndex = index;
-    }
+	public final int targetIndex;
 
-    @Override
-    public CommandResult execute() {
-        FilteredList<Item> lastShownList = model.getFilteredEditableItemList();
-        if (lastShownList.size() < targetIndex) {
-            indicateAttemptToExecuteIncorrectCommand();
+	public DoneCommand(int index) {
+		this.targetIndex = index;
+	}
+
+	private Item itemToUndone;
+
+	@Override
+	public CommandResult execute() {
+		FilteredList<Item> lastShownList = model.getFilteredEditableItemList();
+		if (lastShownList.size() < targetIndex) {
+			indicateAttemptToExecuteIncorrectCommand();
 			return new CommandResult(Messages.MESSAGE_INVALID_ITEM_DISPLAYED_INDEX);
-        }
-        Item itemToComplete = lastShownList.get(targetIndex - 1);
-        
-        if(itemToComplete.getIsDone()) {
-            return new CommandResult(MESSAGE_DONE_ITEM_FAIL);
-        } else {
-        	model.setDoneItem(itemToComplete);
-        }
+		}
+		Item itemToComplete = lastShownList.get(targetIndex - 1);
+
+		if (itemToComplete.getIsDone()) {
+			itemToUndone = null;
+			return new CommandResult(MESSAGE_DONE_ITEM_FAIL);
+		} else {
+			itemToUndone = itemToComplete;
+			model.setDoneItem(itemToComplete);
+		}
 
 		return new CommandResult(MESSAGE_DONE_ITEM_SUCCESS, itemToComplete);
-    }
+	}
 
 	@Override
 	public CommandResult undo() {
-		// TODO Auto-generated method stub
-		return null;
+		if (itemToUndone == null) {
+			return new CommandResult(MESSAGE_UNDO_FAILURE);
+		} else {
+			model.setNotDoneItem(itemToUndone);
+			return new CommandResult(MESSAGE_UNDO_SUCCESS, itemToUndone);
+		}
 	}
-
 
 }
