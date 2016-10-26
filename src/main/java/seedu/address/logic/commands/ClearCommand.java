71426cb68e7ddcf203367701f335d17408a8a4be
acuodancer <leminhduc74@gmail.com>
2016-10-26 20:18:40 +0800

    Add undo() method to Clear command that add back a saved TaskBook state before clearing

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -1,8 +1,9 @@
 package seedu.address.logic.commands;
 
-import javafx.collections.ObservableList;
+import java.util.List;
+
 import seedu.address.model.TaskBook;
-import seedu.address.model.item.Item;
+import seedu.address.model.item.ReadOnlyItem;
 
 /**
  * Clears the address book.
@@ -12,10 +13,11 @@ public class ClearCommand extends Command {
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_SUCCESS = "To-do list has been cleared!";
     
-    private final ObservableList<Item> itemsToAddBack;
+    private final List<ReadOnlyItem> itemsToAddBack;
 
     public ClearCommand() {
-    	itemsToAddBack = TaskBook.getItems();
+    	itemsToAddBack = model.getTaskBook().getItemList();
+    	
     }
     
     @Override
@@ -24,4 +26,13 @@ public class ClearCommand extends Command {
         model.resetData(TaskBook.getEmptyTaskBook());
         return new CommandResult(MESSAGE_SUCCESS);
     }
+
+	@Override
+	public CommandResult undo() {
+		TaskBook savedTaskBook = new TaskBook();
+    	savedTaskBook.resetData(itemsToAddBack);
+		model.resetData(savedTaskBook);
+		return null;
+	}
+    
 }
