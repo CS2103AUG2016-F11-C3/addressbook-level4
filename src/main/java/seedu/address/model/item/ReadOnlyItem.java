35b1f11e938fee13bf77ce42ee6f49132d93d9d7
Yuchuan <yyc478@gmail.com>
2016-10-28 04:50:21 +0800

    Add tags displaying for individual item cards

--- src/main/java/seedu/address/model/item/ReadOnlyItem.java
+++ src/main/java/seedu/address/model/item/ReadOnlyItem.java
@@ -45,8 +45,8 @@ public interface ReadOnlyItem {
 	default boolean isSameStateAs(ReadOnlyItem other) {
 		return this == other
 				|| (other != null
-				&& this.getDescription().equals(((ReadOnlyItem) other).getDescription()))
-		        && this.getTags().equals(((ReadOnlyItem) other).getTags());
+				&& this.getDescription().equals(other.getDescription()))
+		        && this.getTags().equals(other.getTags());
 	}
 
 	/**
@@ -80,4 +80,6 @@ public interface ReadOnlyItem {
 	String extractPrettyItemCardDateTime();
 
 	String extractPrettyRelativeEndDateTime();
+
+	public String getType();
 }
