e87e2ccdafa963e62e6099709a5daf07ed37c233
Cara Leong <heycraa@gmail.com>
2016-10-28 04:31:56 +0800

    Get rid of leftover debugging stuff

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -18,8 +18,8 @@ import seedu.address.model.item.UniqueItemList;
 import seedu.address.model.item.UniqueItemList.ItemNotFoundException;
 
 /**
- * Represents the in-memory model of the address book data.
- * All changes to any model should be synchronized.
+ * Represents the in-memory model of the address book data. All changes to any
+ * model should be synchronized.
  */
 public class ModelManager extends ComponentManager implements Model {
     private static final Logger logger = LogsCenter.getLogger(ModelManager.class);
@@ -29,8 +29,8 @@ public class ModelManager extends ComponentManager implements Model {
     private Predicate defaultPredicate;
 
     /**
-     * Initializes a ModelManager with the given AddressBook
-     * AddressBook and its variables should not be null
+     * Initializes a ModelManager with the given AddressBook AddressBook and its
+     * variables should not be null
      */
     public ModelManager(TaskBook src, UserPrefs userPrefs) {
         super();
@@ -85,19 +85,21 @@ public class ModelManager extends ComponentManager implements Model {
         updateFilteredListToShowAll();
         indicateTaskBookChanged();
     }
-    
+
     @Override
     public synchronized void doneItem(ReadOnlyItem item) {
-    	item.setIsDone(true);
+        item.setIsDone(true);
         updateFilteredListToShowAll();
         indicateTaskBookChanged();
     }
 
-    //=========== Filtered Item List Accessors ===============================================================
+    // =========== Filtered Item List Accessors
+    // ===============================================================
 
     @Override
     /**
      * Returns a list sorted chronologically
+     * 
      * @@author A0131560U
      */
     public UnmodifiableObservableList<ReadOnlyItem> getFilteredItemList() {
@@ -108,36 +110,36 @@ public class ModelManager extends ComponentManager implements Model {
     public void updateFilteredListToShowAll() {
         filteredItems.setPredicate(null);
     }
-    
+
     @Override
-    public void updateFilteredListDefaultPredicate(String taskType){
+    public void updateFilteredListDefaultPredicate(String taskType) {
         defaultPredicate = new QualifierPredicate(new TypeQualifier(taskType));
         updateFilteredItemList(defaultPredicate);
     }
 
     @Override
-    public void updateFilteredItemList(Set<String> keywords){
-		updateFilteredItemList(new QualifierPredicate(new KeywordQualifier(keywords))
-		        .and(defaultPredicate));
+    public void updateFilteredItemList(Set<String> keywords) {
+        updateFilteredItemList(new QualifierPredicate(new KeywordQualifier(keywords)).and(defaultPredicate));
     }
 
-	private void updateFilteredItemList(Predicate pred) {
-		// Not used, to narrow searches the user has to type the entire search
-		// string in
-		// if(filteredItems.getPredicate() != null){
-		// filteredItems.setPredicate(pred.and(filteredItems.getPredicate()));
-		// } else{
-		filteredItems.setPredicate(pred);
-		// }
+    private void updateFilteredItemList(Predicate pred) {
+        // Not used, to narrow searches the user has to type the entire search
+        // string in
+        // if(filteredItems.getPredicate() != null){
+        // filteredItems.setPredicate(pred.and(filteredItems.getPredicate()));
+        // } else{
+        filteredItems.setPredicate(pred);
+        // }
     }
 
-    //========== Inner classes/interfaces used for filtering ==================================================
+    // ========== Inner classes/interfaces used for filtering
+    // ==================================================
 
-	private class QualifierPredicate implements Predicate<ReadOnlyItem> {
+    private class QualifierPredicate implements Predicate<ReadOnlyItem> {
 
         private final Qualifier qualifier;
 
-		QualifierPredicate(Qualifier qualifier) {
+        QualifierPredicate(Qualifier qualifier) {
             this.qualifier = qualifier;
         }
 
@@ -146,45 +148,44 @@ public class ModelManager extends ComponentManager implements Model {
             return qualifier.toString();
         }
 
-		@Override
-		public boolean test(ReadOnlyItem item) {
-			return qualifier.run(item);
-		}
+        @Override
+        public boolean test(ReadOnlyItem item) {
+            return qualifier.run(item);
+        }
 
     }
 
     interface Qualifier {
         boolean run(ReadOnlyItem item);
+
         @Override
-		String toString();
+        String toString();
     }
-    
+
     private class TypeQualifier implements Qualifier {
         private String type;
-        
+
         TypeQualifier(String type) {
             this.type = type;
         }
 
         @Override
         public boolean run(ReadOnlyItem item) {
-            if(!item.is(type)){
-                System.out.println(item.getAsText() + " is not a \"" + type + "\"");
+            if (!item.is(type)) {
                 return false;
             }
             return true;
         }
-        
+
         @Override
-        public String toString(){
+        public String toString() {
             return "type= " + type;
         }
-        
-        
+
     }
 
     private class KeywordQualifier implements Qualifier {
-		private Set<String> searchKeyWords;
+        private Set<String> searchKeyWords;
 
         KeywordQualifier(Set<String> nameKeyWords) {
             this.searchKeyWords = nameKeyWords;
@@ -192,12 +193,12 @@ public class ModelManager extends ComponentManager implements Model {
 
         @Override
         public boolean run(ReadOnlyItem item) {
-        	for (String keyword: searchKeyWords){
-        		if(!new Keyword(keyword).search(item)){
-        			return false;
-        		}
-        	}
-			return true;
+            for (String keyword : searchKeyWords) {
+                if (!new Keyword(keyword).search(item)) {
+                    return false;
+                }
+            }
+            return true;
         }
 
         @Override
@@ -206,30 +207,29 @@ public class ModelManager extends ComponentManager implements Model {
         }
     }
 
-	// Idea @@author A0092390E
-	private class Keyword {
-		private String keyword;
+    // Idea @@author A0092390E
+    private class Keyword {
+        private String keyword;
 
-		Keyword(String _keyword) {
-			keyword = _keyword;
-		}
+        Keyword(String _keyword) {
+            keyword = _keyword;
+        }
 
-		public boolean search(ReadOnlyItem item){
-			if(keyword.matches(Parser.COMMAND_DESCRIPTION_REGEX)){
-				return StringUtil.containsIgnoreCase(item.getDescription().getFullDescription(), 
-				        keyword.replace(Parser.COMMAND_DESCRIPTION_PREFIX, ""));
-			} else if (keyword.matches(Parser.COMMAND_TAG_REGEX)){
-				return StringUtil.containsIgnoreCase(item.getTags().listTags(), 
-				        keyword.replaceFirst(Parser.COMMAND_TAG_PREFIX, ""));
-			}
-			else {
-			    DateTimeParser parseDate = new DateTimeParser(keyword);
-			    return ((item.getStartDate() != null
-			            && DateTimeParser.isSameDay(item.getStartDate(), parseDate.extractStartDate())
-			            || (item.getEndDate() != null
-			            && DateTimeParser.isSameDay(item.getEndDate(),parseDate.extractStartDate()))));
-			}
-		}
-	}
+        public boolean search(ReadOnlyItem item) {
+            if (keyword.matches(Parser.COMMAND_DESCRIPTION_REGEX)) {
+                return StringUtil.containsIgnoreCase(item.getDescription().getFullDescription(),
+                        keyword.replace(Parser.COMMAND_DESCRIPTION_PREFIX, ""));
+            } else if (keyword.matches(Parser.COMMAND_TAG_REGEX)) {
+                return StringUtil.containsIgnoreCase(item.getTags().listTags(),
+                        keyword.replaceFirst(Parser.COMMAND_TAG_PREFIX, ""));
+            } else {
+                DateTimeParser parseDate = new DateTimeParser(keyword);
+                return ((item.getStartDate() != null
+                        && DateTimeParser.isSameDay(item.getStartDate(), parseDate.extractStartDate())
+                        || (item.getEndDate() != null
+                                && DateTimeParser.isSameDay(item.getEndDate(), parseDate.extractStartDate()))));
+            }
+        }
+    }
 
 }
