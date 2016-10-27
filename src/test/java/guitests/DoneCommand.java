516fe11542f7aef7bfff46f749eb47691c919cec
acuodancer <leminhduc74@gmail.com>
2016-10-27 17:04:01 +0800

    Add DoneCommand test class (no cases yet)

---
+++ src/test/java/guitests/DoneCommand.java
@@ -0,0 +1,24 @@
+package guitests;
+
+import guitests.guihandles.ItemCardHandle;
+import org.junit.Test;
+import seedu.address.logic.commands.UndoCommand;
+import seedu.address.testutil.TestItem;
+import seedu.address.testutil.TestUtil;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * GUI test for undo commands for each: Add, Delete, Clear, Done and Edit command
+ * @author Darren Le
+ * @@author A0144750J
+ */
+public class DoneCommand extends TaskBookGuiTest {
+
+    @Test
+    public void done() {
+        // add one item
+        
+
+    }
+    
+}
