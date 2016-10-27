52d92a4305437c09ecfd32c69197382ffbcc2973
acuodancer <leminhduc74@gmail.com>
2016-10-27 21:52:59 +0800

    Add basic test to mark an item as done and check

--- src/test/java/guitests/DoneCommandTest.java
+++ src/test/java/guitests/DoneCommandTest.java
@@ -1,8 +1,7 @@
 package guitests;
 
-import guitests.guihandles.ItemCardHandle;
 import org.junit.Test;
-import seedu.address.logic.commands.UndoCommand;
+import seedu.address.logic.commands.DoneCommand;
 import seedu.address.testutil.TestItem;
 import seedu.address.testutil.TestUtil;
 import static org.junit.Assert.assertTrue;
@@ -12,13 +11,31 @@ import static org.junit.Assert.assertTrue;
  * @author Darren Le
  * @@author A0144750J
  */
-public class DoneCommand extends TaskBookGuiTest {
+public class DoneCommandTest extends TaskBookGuiTest {
 
     @Test
     public void done() {
-        // add one item
-        
+        // mark one item as done
+        TestItem[] currentList = td.getTypicalItems();
+        int targetIndex = 1;
+        assertDoneSuccess(targetIndex, currentList);
+    }
+    
+    /**
+     * 
+     * @param targetIndexOneIndexed e.g. to delete the first person in the list, 1 should be given as the target index.
+     * @param currentList A copy of the current list of persons (before deletion).
+     */
+    private void assertDoneSuccess(int targetIndexOneIndexed, final TestItem[] currentList) {
+        TestItem[] expectedRemainder = TestUtil.doneItemFromList(currentList, targetIndexOneIndexed);
 
+        commandBox.runCommand("done " + targetIndexOneIndexed);
+
+        //confirm the list now contains all previous persons except the deleted person
+        assertTrue(itemListPanel.isListMatching(expectedRemainder));
+
+        //confirm the result message is correct
+        assertResultMessage(DoneCommand.MESSAGE_DONE_ITEM_SUCCESS);
     }
     
 }
