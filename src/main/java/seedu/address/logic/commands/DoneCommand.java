f355143c263c3fabd275b31d3379395391d98133
acuodancer <leminhduc74@gmail.com>
2016-10-27 15:32:56 +0800

    Display item details in MESSAGE_UNDO_FAILURE

--- src/main/java/seedu/address/logic/commands/DoneCommand.java
+++ src/main/java/seedu/address/logic/commands/DoneCommand.java
@@ -10,11 +10,8 @@ public class DoneCommand extends Command {
 	public static final String COMMAND_WORD = "done";
 
 	public static final String MESSAGE_UNDO_SUCCESS = "Undo set done task: %1$s";
-	public static final String MESSAGE_UNDO_FAILURE = "";
-
 	public static final String MESSAGE_USAGE = COMMAND_WORD + ": Marks the task identified by index as done.\n"
 			+ "Parameters: INDEX (must be a positive integer)\n" + "Example 1: " + COMMAND_WORD + " 1\n";
-
 	public static final String MESSAGE_DONE_ITEM_SUCCESS = "Task marked as complete!";
 	public static final String MESSAGE_DONE_ITEM_FAIL = "Task already marked as complete!";
 
@@ -50,7 +47,7 @@ public class DoneCommand extends Command {
 	public CommandResult undo() {
 		assert itemToUndone != null;
 		model.setNotDoneItem(itemToUndone);
-		return new CommandResult(MESSAGE_UNDO_SUCCESS, itemToUndone);
+		return new CommandResult(String.format(MESSAGE_UNDO_SUCCESS, itemToUndone), itemToUndone);
 	}
 
 }
