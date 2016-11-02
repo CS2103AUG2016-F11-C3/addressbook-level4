eb6811cfb9f99f13de77e97070f2b2ff67281ee2
acuodancer <leminhduc74@gmail.com>
2016-11-02 22:48:01 +0800

    Add event handlers method for paging up down to LogicManager

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -4,6 +4,9 @@ import javafx.collections.ObservableList;
 import seedu.address.commons.core.ComponentManager;
 import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.events.ui.JumpToListRequestEvent;
+import seedu.address.commons.events.ui.ListPageDownEvent;
+import seedu.address.commons.events.ui.ListPageUpEvent;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.parser.Parser;
@@ -14,6 +17,8 @@ import seedu.address.storage.Storage;
 import java.util.Stack;
 import java.util.logging.Logger;
 
+import com.google.common.eventbus.Subscribe;
+
 /**
  * The main LogicManager of the app.
  */
@@ -23,9 +28,13 @@ public class LogicManager extends ComponentManager implements Logic {
     private final Model model;
     private final Parser parser;
 
+    private int currentListIndex;
+    private final int PAGE_STEP = 5;
+    
     public LogicManager(Model model, Storage storage) {
         this.model = model;
         this.parser = new Parser();
+        currentListIndex = 0;
         registerAsHandler();
     }
 
@@ -55,5 +64,25 @@ public class LogicManager extends ComponentManager implements Logic {
     
     private void registerAsHandler() {
         eventsCenter.registerHandler(this);
+    }
+    
+    @Subscribe
+    private void handleListPageUpEvent(ListPageUpEvent event) {
+        if (currentListIndex - PAGE_STEP < 0) {
+            currentListIndex = 0;
+        } else {
+            currentListIndex = currentListIndex - PAGE_STEP;
+        }
+        EventsCenter.getInstance().post(new JumpToListRequestEvent(currentListIndex));
+    }
+    
+    @Subscribe
+    private void handleListPageDownEvent(ListPageDownEvent event) {
+        if (currentListIndex + PAGE_STEP >= model.getFilteredItemList().size()) {
+            currentListIndex = model.getFilteredItemList().size() - 1;
+        } else {
+            currentListIndex = currentListIndex + PAGE_STEP;
+        }
+        EventsCenter.getInstance().post(new JumpToListRequestEvent(currentListIndex));
     }
 }
