ba07521afcf5fc80d23d0255d88ec99f51d60489
acuodancer <leminhduc74@gmail.com>
2016-11-03 12:06:00 +0800

    Create two new events for requesting previous or next command

---
+++ src/main/java/seedu/address/commons/events/ui/NextCommandEvent.java
@@ -0,0 +1,19 @@
+package seedu.address.commons.events.ui;
+
+import seedu.address.commons.events.BaseEvent;
+
+//@@author A0144750J
+/**
+ * Indicates a request to cycle to the next command
+ */
+public class NextCommandEvent extends BaseEvent {
+
+    public NextCommandEvent() {
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+
+}
---
+++ src/main/java/seedu/address/commons/events/ui/PreviousCommandEvent.java
@@ -0,0 +1,19 @@
+package seedu.address.commons.events.ui;
+
+import seedu.address.commons.events.BaseEvent;
+
+//@@author A0144750J
+/**
+ * Indicates a request to cycle to the previous command
+ */
+public class PreviousCommandEvent extends BaseEvent {
+
+    public PreviousCommandEvent() {
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+
+}
