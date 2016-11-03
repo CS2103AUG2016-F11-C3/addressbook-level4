a53092b568e5e7286d3a7b6270fa79ca8f312d6a
Darren Wee <darren.wee@u.nus.edu>
2016-11-04 00:24:14 +0800

    Add messages and constructor for SyncCommand

--- src/main/java/seedu/address/logic/commands/SyncCommand.java
+++ src/main/java/seedu/address/logic/commands/SyncCommand.java
@@ -2,15 +2,28 @@ package seedu.address.logic.commands;
 
 public class SyncCommand extends Command {
 
+    public static final String COMMAND_WORD = "sync";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Syncs items between Sudowudo and a Google Calendar.\n"
+            + "Parameters: " + COMMAND_WORD + " CALENDAR_NAME\n"
+            + "Example: " + COMMAND_WORD + " " + "NUS Timetable\n";
+
+    private final String targetCalendar;
+
+    public SyncCommand(String targetCalendar) {
+        this.targetCalendar = targetCalendar;
+    }
+    
     @Override
     public CommandResult execute() {
-        // TODO Auto-generated method stub
+        assert model != null;
+        
         return null;
     }
 
     @Override
     public CommandResult undo() {
-        // TODO Auto-generated method stub
         return null;
     }
 
