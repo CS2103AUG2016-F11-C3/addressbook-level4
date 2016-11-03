245029be507308396eef14647dbc33e134d8eeac
Darren Wee <darren.wee@u.nus.edu>
2016-11-03 23:27:13 +0800

    Fixed formatting in Initializer

--- src/main/java/seedu/address/gcal/Initializer.java
+++ src/main/java/seedu/address/gcal/Initializer.java
@@ -23,30 +23,31 @@ import java.util.List;
 
 public class Initializer {
     /** Application name. */
-    private static final String APPLICATION_NAME =
-        "Sudowudo-Google Calendar Interface";
+    private static final String APPLICATION_NAME = "Sudowudo-Google Calendar Interface";
 
     /** Directory to store user credentials for this application. */
     private static final java.io.File DATA_STORE_DIR = new java.io.File(
-        System.getProperty("user.home"), ".credentials/calendar-java-quickstart");
+            System.getProperty("user.home"),
+            ".credentials/calendar-java-quickstart");
 
     /** Global instance of the {@link FileDataStoreFactory}. */
     private static FileDataStoreFactory DATA_STORE_FACTORY;
 
     /** Global instance of the JSON factory. */
-    private static final JsonFactory JSON_FACTORY =
-        JacksonFactory.getDefaultInstance();
+    private static final JsonFactory JSON_FACTORY = JacksonFactory
+            .getDefaultInstance();
 
     /** Global instance of the HTTP transport. */
     private static HttpTransport HTTP_TRANSPORT;
 
-    /** Global instance of the scopes required by this quickstart.
+    /**
+     * Global instance of the scopes required by this quickstart.
      *
-     * If modifying these scopes, delete your previously saved credentials
-     * at ~/.credentials/calendar-java-quickstart
+     * If modifying these scopes, delete your previously saved credentials at
+     * ~/.credentials/calendar-java-quickstart
      */
-    private static final List<String> SCOPES =
-        Arrays.asList(CalendarScopes.CALENDAR_READONLY);
+    private static final List<String> SCOPES = Arrays
+            .asList(CalendarScopes.CALENDAR_READONLY);
 
     static {
         try {
@@ -60,25 +61,24 @@ public class Initializer {
 
     /**
      * Creates an authorized Credential object.
+     * 
      * @return an authorized Credential object.
      * @throws IOException
      */
     public static Credential authorize() throws IOException {
         // Load client secrets.
-        InputStream in =
-            Initializer.class.getResourceAsStream("/client_secret.json");
-        GoogleClientSecrets clientSecrets =
-            GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+        InputStream in = Initializer.class
+                .getResourceAsStream("/client_secret.json");
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets
+                .load(JSON_FACTORY, new InputStreamReader(in));
 
         // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow =
-                new GoogleAuthorizationCodeFlow.Builder(
-                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(DATA_STORE_FACTORY)
-                .setAccessType("offline")
-                .build();
-        Credential credential = new AuthorizationCodeInstalledApp(
-            flow, new LocalServerReceiver()).authorize("user");
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                        .setDataStoreFactory(DATA_STORE_FACTORY)
+                        .setAccessType("offline").build();
+        Credential credential = new AuthorizationCodeInstalledApp(flow,
+                new LocalServerReceiver()).authorize("user");
         System.out.println(
                 "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
         return credential;
@@ -86,17 +86,16 @@ public class Initializer {
 
     /**
      * Build and return an authorized Calendar client service.
+     * 
      * @return an authorized Calendar client service
      * @throws IOException
      */
-    public static com.google.api.services.calendar.Calendar
-        getCalendarService() throws IOException {
+    public static com.google.api.services.calendar.Calendar getCalendarService()
+            throws IOException {
         Credential credential = authorize();
         return new com.google.api.services.calendar.Calendar.Builder(
                 HTTP_TRANSPORT, JSON_FACTORY, credential)
-                .setApplicationName(APPLICATION_NAME)
-                .build();
+                        .setApplicationName(APPLICATION_NAME).build();
     }
-    
 
 }
