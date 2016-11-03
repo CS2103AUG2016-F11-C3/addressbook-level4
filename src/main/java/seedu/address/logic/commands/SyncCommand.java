01125043d02c853715840eaf3627efaed308bd12
Darren Wee <darren.wee@u.nus.edu>
2016-11-04 02:05:45 +0800

    Add successful and failed commandresult for sync

--- src/main/java/seedu/address/logic/commands/SyncCommand.java
+++ src/main/java/seedu/address/logic/commands/SyncCommand.java
@@ -11,13 +11,13 @@ public class SyncCommand extends Command {
     public static final String COMMAND_WORD = "sync";
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Syncs items between Sudowudo and a Google Calendar.\n"
-            + "Parameters: " + COMMAND_WORD + " CALENDAR_NAME\n"
-            + "Example: " + COMMAND_WORD + " " + "NUS Timetable\n";
-    public static final String MESSAGE_SUCCESS = "Synchronization successful!";
-    public static final String MESSAGE_FAILURE = "Synchronization failed.";
+            + "Parameters: " + COMMAND_WORD + " CALENDAR_NAME\n" + "Example: "
+            + COMMAND_WORD + " " + "NUS Timetable\n";
+    public static final String MESSAGE_SUCCESS = "Synchronization with \"%1$s\" successful!";
+    public static final String MESSAGE_FAILURE = "Synchronization with \"%1$s\" failed.";
 
     private final String targetCalendar;
-    
+
     // for communicating with the google calendar API
     private static final Interfacer interfacer = new Interfacer();
 
@@ -25,24 +25,32 @@ public class SyncCommand extends Command {
         this.hasUndo = false;
         this.targetCalendar = targetCalendar;
     }
-    
+
     @Override
     public CommandResult execute() {
         assert model != null;
-        
+
+        // catch non-existent calendar
+        if (!interfacer.hasCalendar(this.targetCalendar)) {
+            return new CommandResult(
+                    String.format(MESSAGE_FAILURE, this.targetCalendar) + "\n"
+                            + Interfacer.GCAL_CALENDAR_NOT_FOUND);
+        }
+
         // pull items from remote calendar
         List<Item> toAdd = interfacer.pullItems(this.targetCalendar);
-        for(Item item : toAdd) {
+        for (Item item : toAdd) {
             try {
                 model.addItem(item);
             } catch (DuplicateItemException e) {
                 // do nothing if attempted to add duplicate
             }
         }
-        
+
         // push
-        
-        return null;
+
+        return new CommandResult(
+                String.format(MESSAGE_SUCCESS, this.targetCalendar));
     }
 
     @Override
