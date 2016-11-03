63f87abc41111c717f2b2bbc7c9aa9231a7b8ec6
Darren Wee <darren.wee@u.nus.edu>
2016-11-03 23:26:03 +0800

    Initial commit for SyncCommand

---
+++ src/main/java/seedu/address/logic/commands/SyncCommand.java
@@ -0,0 +1,17 @@
+package seedu.address.logic.commands;
+
+public class SyncCommand extends Command {
+
+    @Override
+    public CommandResult execute() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public CommandResult undo() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
