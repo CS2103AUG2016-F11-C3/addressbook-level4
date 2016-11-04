211ec9c589abcd7106deaf7238f630dba3777e61
Yuchuan <yyc478@gmail.com>
2016-11-04 11:26:21 +0800

    Remove help window test, since we no longer open the windows

--- src/test/java/guitests/HelpWindowTest.java
+++ src/test/java/guitests/HelpWindowTest.java
@@ -1,27 +1,12 @@
 package guitests;
 
-import guitests.guihandles.HelpWindowHandle;
 import org.junit.Test;
-
-import static org.junit.Assert.assertTrue;
 
 public class HelpWindowTest extends TaskBookGuiTest {
 
-    @Test
-    public void openHelpWindow() {
+	@Test
+	public void openHelpPrompt() {
 
-        itemListPanel.clickOnListView();
+	}
 
-        assertHelpWindowOpen(mainMenu.openHelpWindowUsingAccelerator());
-
-        assertHelpWindowOpen(mainMenu.openHelpWindowUsingMenu());
-
-        assertHelpWindowOpen(commandBox.runHelpCommand());
-
-    }
-
-    private void assertHelpWindowOpen(HelpWindowHandle helpWindowHandle) {
-        assertTrue(helpWindowHandle.isWindowOpen());
-        helpWindowHandle.closeWindow();
-    }
 }
