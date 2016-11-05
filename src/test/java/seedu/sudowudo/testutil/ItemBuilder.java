23721c11da6263cb1817d4f111327bd4219f51d8
Darren Wee <darren.wee@u.nus.edu>
2016-11-06 00:16:24 +0800

    Adapt ItemBuilder to singletone DateTimeParser

--- src/test/java/seedu/sudowudo/testutil/ItemBuilder.java
+++ src/test/java/seedu/sudowudo/testutil/ItemBuilder.java
@@ -1,7 +1,5 @@
 package seedu.sudowudo.testutil;
 
-import java.time.LocalDateTime;
-
 import seedu.sudowudo.commons.exceptions.IllegalValueException;
 import seedu.sudowudo.logic.parser.DateTimeParser;
 import seedu.sudowudo.model.item.Description;
@@ -11,38 +9,38 @@ import seedu.sudowudo.model.tag.UniqueTagList;
 public class ItemBuilder {
 
     private TestItem item;
-    
-    public ItemBuilder(){
+    private final DateTimeParser dtparser = DateTimeParser.getInstance();
+
+    public ItemBuilder() {
         this.item = new TestItem();
     }
-    //@@author A0131560U
-    public ItemBuilder withDescription(String description) throws IllegalValueException{
+
+    // @@author A0131560U
+    public ItemBuilder withDescription(String description) throws IllegalValueException {
         this.item.setDescription(new Description(description));
         return this;
     }
-    //@@author A0144750J
-    public ItemBuilder withDates(String startdate) throws IllegalValueException{
-    	DateTimeParser parser = new DateTimeParser(startdate);
-		LocalDateTime startTimeObj = parser.extractStartDate();
-		LocalDateTime endTimeObj = parser.extractStartDate();
-		this.item.setStartDate(startTimeObj);
-		this.item.setEndDate(endTimeObj);
-		return this;
+
+    // @@author A0144750J
+    public ItemBuilder withDates(String startDate) throws IllegalValueException {
+        this.dtparser.parse(startDate);
+        this.item.setStartDate(this.dtparser.extractStartDate());
+        this.item.setEndDate(this.dtparser.extractEndDate());
+        return this;
     }
-    
-    //@@author
-    public ItemBuilder withTags(String... tags) throws IllegalValueException{
-        UniqueTagList replacement = new UniqueTagList(); 
-        for (String tag : tags){
+
+    // @@author
+    public ItemBuilder withTags(String... tags) throws IllegalValueException {
+        UniqueTagList replacement = new UniqueTagList();
+        for (String tag : tags) {
             replacement.add(new Tag(tag));
         }
         this.item.setTags(replacement);
         return this;
     }
 
-    
-    public TestItem build(){
+    public TestItem build() {
         return this.item;
     }
-    
+
 }
