e07718b88f413c77908ff4a8136bd0ea67acbda5
Cara Leong <heycraa@gmail.com>
2016-11-07 22:52:24 +0800

    Get rid of unused clear thingy

--- src/main/java/seedu/sudowudo/logic/commands/FindCommand.java
+++ src/main/java/seedu/sudowudo/logic/commands/FindCommand.java
@@ -3,27 +3,18 @@ package seedu.sudowudo.logic.commands;
 import java.util.Set;
 
 /**
- * Finds and lists all items in task book whose descriptions, tags or start/end dates
- * contains any of the argument keywords.
- * Keyword matching is not case sensitive.
+ * Finds and lists all items in task book whose descriptions, tags or start/end
+ * dates contains any of the argument keywords. Keyword matching is not case
+ * sensitive.
  */
 public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
     public static final String MESSAGE_UNDO_FAILURE = "";
 
-	/**
-	 * never clear the command box, in case the user wants to refine their query
-	 * 
-	 */
-	@Override
-	public boolean ClearOnExecute() {
-		return false;
-	}
-
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all items that match at least "
-            + "one of the given keywords \"[DESCRIPTION_KEYWORD]\", #[TAG_KEYWORD], [TIME_KEYWORD] "
-            + "Example: " + COMMAND_WORD + " \"task\" \"CS2103\" #important tomorrow";
+            + "one of the given keywords \"[DESCRIPTION_KEYWORD]\", #[TAG_KEYWORD], [TIME_KEYWORD] " + "Example: "
+            + COMMAND_WORD + " \"task\" \"CS2103\" #important tomorrow";
 
     private final Set<String> keywords;
 
@@ -33,13 +24,13 @@ public class FindCommand extends Command {
 
     @Override
     public CommandResult execute() {
-    	hasUndo = false;
+        hasUndo = false;
         model.updateFilteredItemList(keywords);
-		CommandResult res = new CommandResult(getMessageForItemListShownSummary(model.getFilteredItemList().size()));
-		res.setClear(false);
-		return res;
+        CommandResult res = new CommandResult(getMessageForItemListShownSummary(model.getFilteredItemList().size()));
+        res.setClear(false);
+        return res;
     }
-    
+
     @Override
     public CommandResult undo() {
         return new CommandResult(MESSAGE_UNDO_FAILURE);
